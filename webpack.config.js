7cbe338db27efdafcfcfc23a774721851e170214
ExtraPolite <imextrapolite2@gmail.com>
2024-07-29 15:40:33 -0400

    fix error in config file

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  input: './src/index.js',
+  entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
